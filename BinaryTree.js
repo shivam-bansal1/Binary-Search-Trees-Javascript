420dfa3b95699387ca3f40dfda867fc5481d9383
Shivam Bansal <shivambnsal@gmail.com>
2024-12-14 23:58:12 +0530

    Implement insert and deleteItem method

--- BinaryTree.js
+++ BinaryTree.js
@@ -25,7 +25,93 @@ class Tree {
     array = [...new Set(array)];
     return this.rootNode(array, 0, array.length - 1);
   }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this.root;
+    }
+
+    let parentNode = null;
+    let currentNode = this.root;
+
+    while (currentNode !== null) {
+      parentNode = currentNode;
+      if (value == currentNode.data) {
+        return this.root;
+      } else if (value > currentNode.data) {
+        currentNode = currentNode.right;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+
+    if (value > parentNode.data) {
+      parentNode.right = newNode;
+    } else {
+      parentNode.left = newNode;
+    }
+
+    return this.root;
+  }
+
+  deleteItem(value, currentNode = this.root, parentNode = null) {
+    if (currentNode === null) {
+      console.log(`${value} not found in the tree.`);
+      return this.root;
+    }
+
+    if (value < currentNode.data) {
+      currentNode.left = this.deleteItem(value, currentNode.left, currentNode);
+    } else if (value > currentNode.data) {
+      currentNode.right = this.deleteItem(
+        value,
+        currentNode.right,
+        currentNode
+      );
+    } else {
+      // Handling 3 different cases
+      // Node is a leaf node
+      if (!currentNode.left && !currentNode.right) {
+        return null;
+      }
+      // Node has both left and right subtree
+      else if (currentNode.left && currentNode.right) {
+        const successor = this.findMinValueNode(currentNode.right);
+        currentNode.data = successor.data;
+        currentNode.right = this.deleteItem(
+          successor.data,
+          currentNode.right,
+          currentNode
+        );
+      }
+      // Node has either left or right subtree
+      else {
+        return currentNode.left || currentNode.right;
+      }
+    }
+
+    return currentNode;
+  }
+
+  findMinValueNode(node) {
+    while (node.left !== null) node = node.left;
+    return node;
+  }
 }
 
-let newTree = new Tree([1, 4, 65, 0, 4, 2, 1, 0]);
+let newTree = new Tree([]);
+newTree.insert(45);
+newTree.insert(15);
+newTree.insert(79);
+newTree.insert(90);
+newTree.insert(10);
+newTree.insert(55);
+newTree.insert(20);
+newTree.insert(50);
+newTree.insert(100);
+newTree.insert(2);
+prettyPrint(newTree.root);
+newTree.deleteItem(100);
 prettyPrint(newTree.root);
